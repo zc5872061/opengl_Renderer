80e2930232be58456795538e7b2a9f778825e304
zc5872061 <chukie@ChukiedeMac-mini.local>
2015-08-07 13:26:50 +0800

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Chukie on 15/8/7.
+//  Copyright (c) 2015年 Chukie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
