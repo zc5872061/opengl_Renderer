b895878b0edc188820d4adb7de5a5838e3da626e
zc5872061 <chukie@ChukiedeMac-mini.local>
2015-08-25 20:10:29 +0800

    first_commit
    first_commit

--- test/main.cpp
+++ test/main.cpp
@@ -5,11 +5,42 @@
 //  Created by Chukie on 15/8/7.
 //  Copyright (c) 2015年 Chukie. All rights reserved.
 //
-
+#define GLFW_INCLUDE_GLCOREARB
 #include <iostream>
+#include <ctime>
+#include <map>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    if (!glfwInit()) {
+        return -1;
+    }
+    glm::vec3 vec;
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello OpenGL", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    cout << "OpenGL Vendor:" << glGetString(GL_VENDOR) << endl;
+    cout << "OpenGL Renderer: " << glGetString(GL_RENDERER) << endl;
+    cout << "OpenGL Version: " << glGetString(GL_VERSION) << endl;
+    cout << "GLSL Version:" << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+    glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
